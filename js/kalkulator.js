9aaa200ac647179a33ed513cf267b60d3d49081d
JovanaRadisavljevic <radisavljevic435@gmail.com>
2025-05-02 18:42:44 +0200

    funkcionalnosti kalkulatora su uspesno implementirane

---
+++ js/kalkulator.js
@@ -0,0 +1,29 @@
+const ekran = document.getElementById("ekran");
+const dugmici = document.querySelectorAll("#dugmici button");
+//2. nacin
+const dugmici2 =document.getElementsByTagName("button");
+
+dugmici.forEach((dugme)=>{
+    dugme.addEventListener("click", ()=>{
+        let vrednost = dugme.innerHTML;
+        if(vrednost==="C"){
+            ekran.value="";
+        }
+        else if(vrednost==="="){
+            try{
+                let rezultat = eval(ekran.value);
+                ekran.value=rezultat;
+            }catch(error){
+                ekran.value="GRESKA!";
+            }
+            
+        }
+        else{
+            ekran.value+=vrednost;
+        }
+    });
+});
+
+
+
+ekran.value= "0";
